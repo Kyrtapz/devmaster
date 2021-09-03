26602363d381c57be5a9685587ac463d0aed7aa9
Patryk Diak <pdiak@redhat.com>
2021-09-03 11:07:38 +0200

    clone custom branch

--- .provisioners/_scripts/dotfiles.sh
+++ .provisioners/_scripts/dotfiles.sh
@@ -3,14 +3,16 @@
 repo="kyrtapz/dotfiles.git"
 repo_https="https://github.com/$repo"
 repo_git="git@github.com:$repo"
+branch="pdiak"
 
 [ -d "${HOME}/.dotfiles" ] && {
     cd ${HOME}/.dotfiles
-    git pull -f --depth 1 "$repo_https"
+    git pull -f --depth 1 "$repo_https" "$branch"
 } || {
-    git clone --depth 1 "$repo_https" ${HOME}/.dotfiles
+    git clone --depth 1 --no-single-branch "$repo_https" ${HOME}/.dotfiles
     cd ${HOME}/.dotfiles
     git remote add upstream "$repo_git"
+    [ -n "$branch" ] && git checkout "$branch"
 }
 
 stow zsh git code-server
